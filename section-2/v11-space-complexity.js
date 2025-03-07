90d60da4e062c9423da41fbdd085c707dc90e63d
aakashrathore <aakash.rathore321@gmail.com>
2025-03-07 15:09:36 +0530

    v11 space complexity

--- section-2/v11-space-complexity.js
+++ section-2/v11-space-complexity.js
@@ -2,8 +2,27 @@
 
 // Rules of thumb
 
+/* Sometimes you'll hear the term auxiliary space complexity to refer to space required by the algorithm, not including the space taken up by the inputs. */
+
 /* 1. Most primitives (booleans, numbers, undefined , null) are constant space.
    2. String require O(n) space (where n is the string length)
    3. Reference types are generally O(n) =, where n is the length (for arrays) or the number of keys (for objects)
    4. 
 */
+
+function sum(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+}
---
+++ section-2/v12-logarithm.js
@@ -0,0 +1,7 @@
+/* Logarithm is inverse of exponent */
+
+/* 
+=> Certain searching algorithms have logarithmic time complexity.
+=> Efficient sorting algorithms involve logarithms.
+=> Recursion sometimes involves logarithmic space complexity.
+*/
