9184bf5bab15100fa1341e3af1dfbc47a5edeb12
aakashrathore <aakash.rathore321@gmail.com>
2025-03-11 08:18:36 +0530

    v-23 charCount

---
+++ section-4-Problem-Solving/v20-problemSolving.js
@@ -0,0 +1,55 @@
+// Problem solving
+// Algorithm is a process or set of steps to accomplish a certain task.
+
+/* 
+1.Understand the problem.
+2.Explore concrete examples.
+3.Break it down.
+4.Solve/simplify
+5.Look back and refactor.
+*/
+
+// Understand the problem
+
+/* 
+1. Can i restate the problem in my own words?
+2. What are the inputs that go in the problem?
+3. What are the outputs that should come from the solution to the problem ?
+4. Can the outputs be determined from the inputs ? In the other words, do i have enough information to solve the problem ?
+(You may not be able to answer this question until you set about solving the problem. That's okay, It's still worth considering the question at this early stage.)
+5. How should i label the important pieces of the data that are a part of the problem?
+*/
+
+/* Write a function which takes in a string and counts each character in a string. */
+
+// function charCount(str) {
+//   const chars = str.replace(/\s+/g, "").toLowerCase().split("");
+//   let result = {};
+//   chars.forEach((el, index) => {
+//     if (result.hasOwnProperty(el)) {
+//       result[el]++;
+//     } else {
+//       result[el] = 1;
+//     }
+//   });
+//   return result;
+// }
+
+// console.log(charCount("This is a character"));
+
+function countChars(str) {
+  let chars = str.replace(/\s+/g, "").toLowerCase().split("");
+  let result = {};
+
+  for (let i = 0; i < chars.length; i++) {
+    let el = chars[i];
+    if (result.hasOwnProperty(el)) {
+      result[el]++;
+    } else {
+      result[el] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(countChars("This is a character"));
---
+++ section-4-Problem-Solving/v23-charCount.js
@@ -0,0 +1,55 @@
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+
+  if (
+    !(code > 47 && code < 58) && //numeric (0-9)
+    !(code > 64 && code < 91) && //upper alpha(A-Z)
+    !(code > 96 && code < 123) //lower alpha (a-z)
+  ) {
+    return false;
+  }
+  return true;
+}
+
+// function charCount(str) {
+//   const chars = str.replace(/\s+/g, "").toLowerCase().split("");
+//   let result = {};
+//   chars.forEach((el, index) => {
+//     if (result.hasOwnProperty(el)) {
+//       result[el]++;
+//     } else {
+//       result[el] = 1;
+//     }
+//   });
+//   return result;
+// }
+
+// const charCount = (str) => {
+//   const result = {};
+
+//   for (let char of str) {
+//     const character = char.toLowerCase();
+//     if (/[a-z0-9]/.test(character)) {
+//       if (result[character] > 0) {
+//         result[character]++;
+//       } else {
+//         result[character] = 1;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+const charCount = (str) => {
+  const result = {};
+
+  for (let char of str) {
+    if (isAlphaNumeric(char)) {
+      const character = char.toLowerCase();
+      result[character] = ++result[character] || 1;
+    }
+  }
+  return result;
+};
+
+console.log(charCount("hello 32166654"));
