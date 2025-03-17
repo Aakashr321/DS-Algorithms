81b7cbe58c7471e0d306e0adad2bba0bed8d257c
aakashrathore <aakash.rathore321@gmail.com>
2025-03-17 11:46:25 +0530

    v43-Recursion

---
+++ section-7-Recursion/v43-Recursion.js
@@ -0,0 +1,13 @@
+/* What is Recursion ? */
+/* 
+A process (a function in our case) that calls itself.
+*/
+
+// JSON.parse and JSON.stringify also uses recursion
+//document.getElementById and DOM traversal algorithms.
+//Object traversal
+
+/* How recursive functions work */
+
+/* Invoke the same function with a different input until
+you reach your base case. */
