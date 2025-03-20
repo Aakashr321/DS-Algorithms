86812d90065a3eabece0921c8e716e09fc28ad9b
aakashrathore <aakash.rathore321@gmail.com>
2025-03-20 18:27:27 +0530

    pure recursion and helper method recursion

--- section-7-Recursion/v43-Recursion.js
+++ section-7-Recursion/v43-Recursion.js
@@ -38,3 +38,11 @@ function sumRange(num) {
 }
 
 console.log(sumRange(5));
+
+function factorial(num) {
+  if (num === 1) return 1;
+
+  return factorial(num - 1) * num;
+}
+
+console.log(factorial(5));
