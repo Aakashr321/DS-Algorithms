9fcc540658e7add156307dc55c36bb99c04b7091
aakashrathore <aakash.rathore321@gmail.com>
2025-03-23 17:23:10 +0530

    bonus recursion

--- section9-Bonus_Recursion/Coding_Exercise_long.js
+++ section9-Bonus_Recursion/Coding_Exercise_long.js
@@ -49,3 +49,34 @@ function capitalizeWords(array) {
 takes in an object and finds all of the values which
 are numbers and converts them to strings. Recursion
 would be a great way to solve this*/
+
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+/* Write a function called collectStrings which 
+ accepts an object and returns an array of all the
+  values in the object that have a typeof string */
+
+function collectStrings(obj) {
+  var stringsArr = [];
+  for (var key in obj) {
+    if (typeof obj[key] === "string") {
+      stringsArr.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+
+  return stringsArr;
+}
