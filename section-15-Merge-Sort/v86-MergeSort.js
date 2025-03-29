cf942c8f7d276a4e52e4f43162a9a48e9dfe874e
aakashrathore <aakash.rathore321@gmail.com>
2025-03-29 15:34:47 +0530

    mergeSort

--- section-15-Merge-Sort/v86-MergeSort.js
+++ section-15-Merge-Sort/v86-MergeSort.js
@@ -83,3 +83,61 @@ function mergeSort(arr) {
 }
 
 console.log(mergeSort([10, 24, 76, 73, 72, 1, 9]));
+
+function merge(arr1, arr2, comparator) {
+  // Default comparator for numbers in ascending order
+  if (!comparator) {
+    comparator = (a, b) => a - b;
+  }
+
+  const mergedArray = [];
+  let i = 0; // Pointer for arr1
+  let j = 0; // Pointer for arr2
+
+  // Traverse both arrays and merge them
+  while (i < arr1.length && j < arr2.length) {
+    if (comparator(arr1[i], arr2[j]) <= 0) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+
+  // If there are remaining elements in arr1, add them
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+
+  // If there are remaining elements in arr2, add them
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArray;
+}
+
+// Example usage:
+var arr1 = [1, 3, 4, 5];
+var arr2 = [2, 4, 6, 8];
+console.log(merge(arr1, arr2)); // [1, 2, 3, 4, 4, 5, 6, 8]
+
+var arr3 = [-2, -1, 0, 4, 5, 6];
+var arr4 = [-3, -2, -1, 2, 3, 5, 7, 8];
+console.log(merge(arr3, arr4)); // [-3, -2, -2, -1, -1, 0, 2, 3, 4, 5, 5, 6, 7, 8]
+
+var arr5 = [3, 4, 5];
+var arr6 = [1, 2];
+console.log(merge(arr5, arr6)); // [1, 2, 3, 4, 5]
+
+var names = ["Bob", "Ethel", "Christine"];
+var otherNames = ["M", "Colt", "Allison", "SuperLongNameOMG"];
+
+function stringLengthComparator(str1, str2) {
+  return str1.length - str2.length;
+}
+
+console.log(merge(names, otherNames, stringLengthComparator)); // ["M", "Bob", "Colt", "Ethel", "Allison", "Christine", "SuperLongNameOMG"]
