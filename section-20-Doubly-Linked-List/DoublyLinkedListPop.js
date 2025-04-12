00e28fc995dd275fd0992c889e11725b891e31f3
aakashrathore <aakash.rathore321@gmail.com>
2025-04-12 21:30:53 +0530

    DoublyLinkedListPop

--- section-20-Doubly-Linked-List/DoublyLinkedListPop.js
+++ section-20-Doubly-Linked-List/DoublyLinkedListPop.js
@@ -31,27 +31,8 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
-  popA() {
-    if (!this.head) return undefined;
-    let current = this.head;
-    let newTail = current;
 
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-
-    this.tail = newTail;
-    this.tail.next = null;
-    this.length--;
-    if (this.length === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return current;
-  }
-
-  popb() {
+  pop() {
     if (!this.head) return undefined;
     let poppedNode = this.tail;
     if (this.length === 0) {
