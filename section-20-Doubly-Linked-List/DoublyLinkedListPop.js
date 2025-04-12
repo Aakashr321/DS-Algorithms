3d8592369799a571684dd28d142e5de3507a4bb9
aakashrathore <aakash.rathore321@gmail.com>
2025-04-12 21:26:45 +0530

    DoublyLinkedListPop

---
+++ section-20-Doubly-Linked-List/DoublyLinkedListPop.js
@@ -0,0 +1,53 @@
+/* Pop : Removing a node from the end of the Doubly linked list.
+Pop Pseudocode:
+=> 
+*/
+
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
