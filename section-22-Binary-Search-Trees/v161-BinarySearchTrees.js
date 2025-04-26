15ee085f739932bc49f557541446fe4e4ac5f458
aakashrathore <aakash.rathore321@gmail.com>
2025-04-26 09:49:03 +0530

    updated

---
+++ section-22-Binary-Search-Trees/v161-BinarySearchTrees.js
@@ -0,0 +1,26 @@
+/* 
+What is a tree ?
+=> A data structure that consists of nodes in a parent/child relationship.
+
+ => Lists are linear as compared to trees which are non-linear.
+ Tree Terminology : 
+ => Root : The top node in a tree
+ => Child : A node directly connected to another node when moving away from the Root.
+ => Parent : The converse notion f a child.
+ => Siblings : A group of nodes with the same parent.
+ => Leaf : A node with no children.
+ => Edge : The connection between one node and another. 
+ > Application of Trees :
+  >HTML DOM
+  >Network Routing
+  >Abstract Syntax tree
+  >Artificial Intelligence
+  >Folders in OS.
+*/
+
+/* 
+How Binary Search Tree (BST) works ?
+=> Every parent node has at most two children.
+=> Every node to the left of a parent node is always less than the parent.
+=> Every node to the right of a parent node is always greater than the parent.
+*/
