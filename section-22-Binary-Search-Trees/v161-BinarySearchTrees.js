dc17774c812475c37973aa0ae5064c382d5fa734
aakashrathore <aakash.rathore321@gmail.com>
2025-04-27 17:05:26 +0530

    BinarySearchTreeInsert

--- section-22-Binary-Search-Trees/v161-BinarySearchTrees.js
+++ section-22-Binary-Search-Trees/v161-BinarySearchTrees.js
@@ -24,3 +24,26 @@ How Binary Search Tree (BST) works ?
 => Every node to the left of a parent node is always less than the parent.
 => Every node to the right of a parent node is always greater than the parent.
 */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+tree.root.left.right = new Node(9);
+
+console.log(tree.root);
